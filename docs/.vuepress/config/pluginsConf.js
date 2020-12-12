a0bfb3e1c94e43e09675aaa0ee7be84e04e7ea9f
王冬 <1776787598@qq.com>
2020-12-12 16:40:49 +0800

    feat:隐藏私密信息

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-
+const secret = require('./secret');
 moment.locale('zh-cn');
 module.exports = {
   '@vuepress/last-updated': {
@@ -17,16 +17,16 @@ module.exports = {
   '@vssue/vuepress-plugin-vssue': {
     // 设置 `platform` 而不是 `api`
     platform: 'github-v4',
-
+    clientId: secret.clientId,
+    clientSecret: secret.clientSecret,
     // 其他的 Vssue 配置
     owner: 'html546',
     repo: 'docs',
-    clientId: 'c74bee7d94f21a063aba',
-    clientSecret: '3bfa42b1768d3e032f735e8e4a1be2f062ba1013',
+
     autoCreateIssue: true
   },
   '@vuepress/back-to-top': true,
   '@vuepress/google-analytics': {
-    'ga': 'G-VLG3R720W0'
+    'ga': secret.ga
   }
 }
