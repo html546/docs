faac04c294279393e848b26d9d17bc8ea58f6e11
王冬 <1776787598@qq.com>
2020-12-12 14:41:20 +0800

    123123

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,15 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: 'assets/img/favicon.ico' }],
     ['meta', { name: 'author', content: '飞跃高山与大洋的鱼' }],
-    ['meta', { name: 'keywords', content: 'vuepress介绍,vuepress说明,飞跃高山与大洋的鱼' }]
+    ['meta', { name: 'keywords', content: 'vuepress介绍,vuepress说明,飞跃高山与大洋的鱼' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/android-chrome-192x192.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/android-chrome-512x512.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
     [
