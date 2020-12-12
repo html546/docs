7e86c3fe5f4b9fbd85b1f82fe5ab0e760aec06b9
王冬 <1776787598@qq.com>
2020-12-12 10:27:34 +0800

    学习vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/hero.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/about' },
+      {
+        text: 'Languages',
+        items: [
+          {
+            text: 'Group1', items: [
+              { text: 'Home', link: '/' },
+              { text: 'Guide', link: '/about' },
+            ]
+          },
+          {
+            text: 'Group2', items: [
+              { text: 'Home', link: '/' },
+              { text: 'Guide', link: '/about' },
+            ]
+          }
+        ]
+      },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: {
+      "/css/": [
+        'c-aaa',
+        'c-bbb',
+        'c-ccc',
+      ],
+      "/javascript/": [
+        'j-aaa',
+        'j-bbb',
+        'j-ccc',
+      ],
+    }
+  }
+}
